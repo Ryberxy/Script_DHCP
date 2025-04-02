f5a7b438c06f251c18098bbf70705e11de7b99ae
Ryberxy <gagomartin2006@gmail.com>
2025-04-02 11:09:12 +0200

    Update dhcpsc.sh

--- dhcpsc.sh
+++ dhcpsc.sh
@@ -23,17 +23,23 @@ function borrar_dependencias
 echo "Solo responde 'si', si tienes paquetes rotos o tenías ya instalado 'isc-dhcp-server' y quieres empezar la instalacón de 0, si no es el caso responde no a continuación"
 read -p "¿Quieres borrar las dependencias del paquete 'isc-dhcp-server'? (si/no)" respuesta
 if [[ $respuesta == 'si' ]] then
-apt-get remove --purge isc-dhcp-server -y 
-apt-get autoremove -y
-clear
-echo "Dependencias borradas."
+  apt-get remove --purge isc-dhcp-server -y 
+  apt-get autoremove -y
+  clear
+  echo "Dependencias borradas."
+else
+  return 
+fi
 
 function instalar_dhcpserver
 echo "Si tienes instalado el paquete 'isc-dhcp-server' responde 'no', en caso contrario responde 'si' (si/no)"
 read -p "¿Quieres instalar 'isc-dhcp-server'?" respuesta2
 if [[ $respuesta2 == 'si' ]] then
-echo "Instalando el servidor DHCP..."
-apt-get install isc-dhcp-server
+  echo "Instalando el servidor DHCP..."
+  apt-get install isc-dhcp-server
+else
+  return
+fi
 
 function f_soyroot(){
   if [[ $UID -eq 0  ]] ;then
