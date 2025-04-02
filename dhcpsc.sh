0d05a587a5837363b1fa014a823a920f972dd563
Ryberxy <gagomartin2006@gmail.com>
2025-04-02 12:53:00 +0200

    Update dhcpsc.sh

--- dhcpsc.sh
+++ dhcpsc.sh
@@ -19,27 +19,30 @@ if [[ $respuesta == 'si' ]] then
 
 #-----------------------------------------------------------------------------------------------------------------------------------------
 #ZONAFUNCIONES
-function borrar_dependencias
+function f_borrar_dependencias(){
 echo "Solo responde 'si', si tienes paquetes rotos o tenías ya instalado 'isc-dhcp-server' y quieres empezar la instalacón de 0, si no es el caso responde no a continuación"
-read -p "¿Quieres borrar las dependencias del paquete 'isc-dhcp-server'? (si/no)" respuesta
-if [[ $respuesta == 'si' ]] then
-  apt-get remove --purge isc-dhcp-server -y 
-  apt-get autoremove -y
-  clear
-  echo "Dependencias borradas."
-else
-  return 
-fi
+read -p "¿Quieres borrar las dependencias del paquete 'isc-dhcp-server'? (si/no): " respuesta
+  if [[ $respuesta == 'si' ]] then
+     apt-get remove --purge isc-dhcp-server -y 
+     apt-get autoremove -y
+     clear
+     echo "Dependencias borradas."
+  else
+     return 
+  fi
+}
 
-function instalar_dhcpserver
+function f_instalar_dhcpserver(){
 echo "Si tienes instalado el paquete 'isc-dhcp-server' responde 'no', en caso contrario responde 'si' (si/no)"
-read -p "¿Quieres instalar 'isc-dhcp-server'?" respuesta2
-if [[ $respuesta2 == 'si' ]] then
-  echo "Instalando el servidor DHCP..."
-  apt-get install isc-dhcp-server
-else
-  return
-fi
+read -p "¿Quieres instalar 'isc-dhcp-server'? (si/no): " respuesta2
+  if [[ $respuesta2 == 'si' ]] then
+     echo "Instalando el servidor DHCP..."
+     apt-get install isc-dhcp-server -y
+     clear
+  else
+     return
+  fi
+}
 
 function f_soyroot(){
   if [[ $UID -eq 0  ]] ;then
@@ -50,3 +53,9 @@ function f_soyroot(){
     return 1
   fi
 }
+
+#Ejecución
+f_soyroot
+apt update && apt upgrade -y
+f_borrar_dependencias
+f_instalar_dhcpserver
