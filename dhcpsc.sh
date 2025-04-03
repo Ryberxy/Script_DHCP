d587d7d1e132d39a55d5089bee2a5a3bfbec7784
aleksanko2025 <aleksanko14@gmail.com>
2025-04-03 09:49:54 +0200

    Añadidas variables colores ANSI

--- dhcpsc.sh
+++ dhcpsc.sh
@@ -6,12 +6,15 @@
 
 
 #DECLARACIONVARIABLES
-
-
-
-
-
-
+# Colores ANSI
+RED="\e[31m"
+GREEN="\e[32m"
+YELLOW="\e[33m"
+BLUE="\e[34m"
+MAGENTA="\e[35m"
+CYAN="\e[36m"
+WHITE="\e[37m"
+RESET="\e[0m"
 
 #-----------------------------------------------------------------------------------------------------------------------------------------
 #ZONAFUNCIONES
